62c1721843b28b1d6614821bc94b4514d71bea0f
Aditya P <pandit.aditya1798@gmail.com>
2024-03-21 09:06:39 +0530

    adding sg.tf

---
+++ sg.tf
@@ -0,0 +1,17 @@
+#security group
+resource "aws_security_group" "Ec2_seq_grp" {
+  name = "Ec2_seq_grp"
+  ingress { /*inbound traffic*/
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress { /*outbound traffic*/
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = { Name = "ec2 seq group" }
+}
